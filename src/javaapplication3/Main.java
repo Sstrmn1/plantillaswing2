0dc876f4cd19b69464970d5f7ba0b47ac5ffecfc
Fabio <sstrmn1@gmail.com>
2023-09-22 13:35:27 -0400

    Commit inicial, plantillas jswing, aspecto sistema operativo windows

---
+++ src/javaapplication3/Main.java
@@ -0,0 +1,30 @@
+package javaapplication3;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Configurar el aspecto de la interfaz Swing (LookAndFeel)
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Crear y configurar el JFrame
+        mostrarFramePrincipal();
+    }
+
+    private static void mostrarFramePrincipal() {
+        // Crear una instancia de NewJFrame
+        NewJFrame frame = new NewJFrame();
+
+        // Centrar la ventana en la pantalla
+        frame.setLocationRelativeTo(null);
+
+        // Hacer la ventana visible
+        frame.setVisible(true);
+    }
+}
